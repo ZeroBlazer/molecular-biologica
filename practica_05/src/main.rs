4b4aadb6449422a4202645e898a1469b52011d3a
Elías Puma <elpumitaelias@gmail.com>
2017-04-15 09:30:47 -0500

    Improvements

--- practica_05/src/main.rs
+++ practica_05/src/main.rs
@@ -7,24 +7,26 @@ fn geometric_distr(p: f32, n: u32) -> f32 {
     diff.powf((n - 1) as f32) * p
 }
 
-fn process_file(p: &str, ctr: &mut [u32; 3]) {
+fn process_file(p: &str) -> (f32, f32, f32) {
     let path = Path::new(p);
-
     let mut file = File::open(&path).expect("Unable to open");
+
     let mut s = String::new();
     file.read_to_string(&mut s).expect("not able to read");
 
-    ctr[2] = s.len() as u32;
+    let mut ctr_0 = 0.0;
+    let mut ctr_1 = 0.0;
 
     for c in s.chars() {
         match c {
-            'A' => ctr[0] += 1,
-            'G' => ctr[0] += 1,
-            'C' => ctr[1] += 1,
-            'T' => ctr[1] += 1,
+            'A' => ctr_0 += 1.0,
+            'G' => ctr_0 += 1.0,
+            'C' => ctr_1 += 1.0,
+            'T' => ctr_1 += 1.0,
             _ => println!("Not a valid letter"),
         }
     }
+    (ctr_0, ctr_1, s.len() as f32)
 }
 
 fn part_a() {
@@ -42,26 +44,23 @@ fn part_a() {
     let mut file = File::create("EPuma_Practica05_A.txt").expect("Couldn't open write file");
 
     for &(x, y) in input.iter() {
-        let val = geometric_distr(x, y);
-        write!(file, "{}\n", val).expect("Couldn't write in file");
+        write!(file, "{val}\n", val = geometric_distr(x, y)).expect("Couldn't write in file");
     }
 }
 
 fn part_b() {
     let path = "practica_05_B.txt";
-    let mut counter: [u32; 3] = [0; 3];
-    process_file(path, &mut counter);
+    let (mut prob_pur, mut prob_pir, length) = process_file(path);
 
-    let prob_pur = counter[0] as f32 / counter[2] as f32;
-    let prob_pir = counter[1] as f32 / counter[2] as f32;
+    prob_pur = prob_pur / length;
+    prob_pir = prob_pir /length;
 
     let input = vec![(prob_pur, 2), (prob_pur, 4), (prob_pir, 3), (prob_pir, 5)];
 
     let mut file = File::create("EPuma_Practica05_B.txt").expect("Couldn't open write file");
 
     for &(x, y) in input.iter() {
-        let val = geometric_distr(x, y);
-        write!(file, "{}\n", val).expect("Couldn't write in file");
+        write!(file, "{}\n", geometric_distr(x, y)).expect("Couldn't write in file");
     }
 }
 
