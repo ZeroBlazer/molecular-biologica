6b109ae85c5af920cc4be880f66406f3ed454a0c
Elías Puma <elpumitaelias@gmail.com>
2017-05-03 16:50:29 -0500

    Sale xD

--- practica_08/src/main.rs
+++ practica_08/src/main.rs
@@ -9,6 +9,16 @@ enum Aminoacid {
     Undef,
 }
 
+#[derive(Debug)]
+enum Direction {
+    Diagonal,
+    Up,
+    Left,
+    Undef,
+}
+
+use Direction::*;
+
 static SCORES: [[i32; 4]; 4] = [[10, -1, -3, -4],
                                 [-1, 7, -5, -3],
                                 [-3, -5, 9, 0],
@@ -34,7 +44,7 @@ fn get_score(a: char, b: char) -> i32 {
     SCORES[b_pos as usize][a_pos as usize]
 }
 
-fn print_matrix(sec_1: &str, sec_2: &str, mtrx: Vec<Vec<i32>>) {
+fn print_matrix(mtrx: &Vec<Vec<i32>>) {
     for row in mtrx.into_iter() {
         // println!("{:?}", row);
         for elem in row.into_iter() {
@@ -69,32 +79,102 @@ fn align_secuence(sec_1: &str, sec_2: &str) {
     let mut uppr_val;
 
     let mut i = 1;
+    let mut j = 1;
     for c2 in sec_2.chars() {
-        let mut j = 1;
+        j = 1;
         for c1 in sec_1.chars() {
             let score = get_score(c2, c1);
             diag_val = score + matrix[i - 1][j - 1];
             left_val = -5 + matrix[i][j - 1];
             uppr_val = -5 + matrix[i - 1][j];
             matrix[i][j] = max(diag_val, max(left_val, uppr_val));
-            // println!("[{}][{}]: {}\td: {}\tl: {}\tu: {}\t->\tval: {}",
-            //          i,
-            //          j,
-            //          score,
-            //          diag_val,
-            //          left_val,
-            //          uppr_val,
-            //          matrix[i][j]);
             j += 1;
         }
         i += 1;
     }
 
-    print_matrix(sec_1, sec_2, matrix);
+    print_matrix(&matrix);
+
+    /************** Alignment **************/
+    let mut align = String::new();
+
+    i = sec_2.len();
+    j = sec_1.len();
+
+    // println!("{}", sec_2.as_bytes()[i] as char);
+    // println!("{}", sec_1.as_bytes()[j] as char);
+
+    let mut direct = Diagonal;
+    let mut greater;
+    loop {
+        print!("i: {} j: {} -> ", i, j);
+        if i == 0 || j == 0 {
+            break;
+        }
+
+        diag_val = matrix[i - 1][j - 1];
+        left_val = matrix[i][j - 1];
+        uppr_val = matrix[i - 1][j];
+        greater = max(diag_val, max(left_val, uppr_val));
+
+        println!("{}", greater);
+
+        match direct {
+            Diagonal => {
+                align.push(sec_2.as_bytes()[i - 1] as char);
+            }
+            Up => {
+                align.push('_');
+            }
+            Left => {
+                align.push('_');
+            }
+            Undef => {}
+        }
+
+        if greater == diag_val {
+            direct = Diagonal;
+            i -= 1;
+            j -= 1;
+        } else if greater == left_val {
+            direct = Left;
+            j -= 1;
+        } else if greater == uppr_val {
+            direct = Up;
+            i -= 1;
+        }
+    }
+    // loop {
+    //     if i == 0 || j == 0 {
+    //         break;
+    //     }
+
+    //     diag_val = matrix[i - 1][j - 1];
+    //     left_val = matrix[i][j - 1];
+    //     uppr_val = matrix[i - 1][j];
+    //     greater = max(diag_val, max(left_val, uppr_val));
+
+    //     println!("{}", greater);
+
+    //     if greater == diag_val {
+    //         println!("diag");
+    //         align.push(sec_2.as_bytes()[i - 1] as char);
+    //         if i > 0 {  i -= 1; }
+    //         if j > 0 {  j -= 1; }
+    //     } else if greater == left_val {
+    //         println!("left");
+    //         align.push('_');
+    //         if i > 0 {  i -= 1; }
+    //     } else if greater == uppr_val {
+    //         println!("up");
+    //         align.push('_');
+    //         if j > 0 {  j -= 1; }
+    //     }
+    // }
+    println!("\nAlignment: {}", align.chars().rev().collect::<String>());
+    /**************************************/
 }
 
 fn main() {
     align_secuence("ACCGTCTT", "CGTCTT");
-    // align_secuence("ACC", "CGT");
-    // println!("Hello, world!");
 }
