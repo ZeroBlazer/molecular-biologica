3acc7edc9e65a9bab5da40cbb15e55578063bb25
Elías Puma <elpumitaelias@gmail.com>
2017-05-31 09:14:55 -0500

    Starting to introduce a generic

--- libs/alignments/src/lib.rs
+++ libs/alignments/src/lib.rs
@@ -18,8 +18,8 @@ impl Clone for Direction {
 }
 
 #[derive(Debug, Clone)]
-struct Score {
-    val: i32,
+struct Score<T> {
+    val: T,
     dir: Direction,
 }
 
@@ -33,7 +33,7 @@ impl Score {
 
 // use tps_tree::{TpsTree, TpsJoint};
 use Direction::*;
-use std::cmp::min;
+use std::cmp::{max, min};
 
 fn get_hsp(w1: &String, w2: &String, match_scr: i32, mism_scr: i32, gap_scr: i32) -> i32 {
     let mut hsp = 0;
@@ -76,13 +76,11 @@ fn print_matrix(seq_vec1: &Vec<String>, seq_vec2: &Vec<String>, mtrx: &Vec<Vec<S
     }
 }
 
-fn align_seqs(mut seq_vec1: Vec<String>, mut seq_vec2: Vec<String>) {
+fn align_seqs_seq(mut seq_vec: Vec<String>, mut seq: String) {
     /********* Initialize sequences **********/
-    for sec in seq_vec1.iter_mut() {
-        *sec = String::from("_") + sec.as_ref();
-    }
+    seq = String::from("_") + seq.as_ref();
 
-    for sec in seq_vec2.iter_mut() {
+    for sec in seq_vec.iter_mut() {
         *sec = String::from("_") + sec.as_ref();
     }
     /********* score function **********/
@@ -96,15 +94,15 @@ fn align_seqs(mut seq_vec1: Vec<String>, mut seq_vec2: Vec<String>) {
         }
     }
     /********* Initialize matrix **********/
-    let len_1 = seq_vec1[0].len();
-    let len_2 = seq_vec2[0].len();
+    let len_1 = seq_vec[0].len();
+    let len_2 = seq.len();
 
     let mut matrix: Vec<Vec<Score>> = vec![vec![Score::new(); len_1]; len_2];
 
     let mut val = 0;
     for j in 1..len_1 {
-        for sec in seq_vec1.iter() {
-            val += score(seq_vec2.chars().nth(0).unwrap(), sec.chars().nth(j).unwrap());
+        for sec in seq_vec.iter() {
+            val += score(seq.chars().nth(0).unwrap(), sec.chars().nth(j).unwrap());
         }
         matrix[0][j].val = val;
         matrix[0][j].dir = Left;
@@ -112,8 +110,8 @@ fn align_seqs(mut seq_vec1: Vec<String>, mut seq_vec2: Vec<String>) {
 
     val = 0;
     for i in 1..len_2 {
-        for sec in seq_vec1.iter() {
-            val += score(seq_vec2.chars().nth(i).unwrap(), sec.chars().nth(0).unwrap());
+        for sec in seq_vec.iter() {
+            val += score(seq.chars().nth(i).unwrap(), sec.chars().nth(0).unwrap());
         }
         matrix[i][0].val = val;
         matrix[i][0].dir = Up;
@@ -128,8 +126,8 @@ fn align_seqs(mut seq_vec1: Vec<String>, mut seq_vec2: Vec<String>) {
         for j in 1..len_1 {
             diag_val = {
                 let mut sum = 0;
-                for sec in seq_vec1.iter() {
-                    sum += score(seq_vec2.chars().nth(i).unwrap(), sec.chars().nth(j).unwrap());
+                for sec in seq_vec.iter() {
+                    sum += score(seq.chars().nth(i).unwrap(), sec.chars().nth(j).unwrap());
                 }
                 sum += matrix[i - 1][j - 1].val;
                 sum
@@ -137,8 +135,8 @@ fn align_seqs(mut seq_vec1: Vec<String>, mut seq_vec2: Vec<String>) {
 
             left_val = {
                 let mut sum = 0;
-                for _ in seq_vec1.iter() {
-                    sum += score(seq_vec2.chars().nth(i).unwrap(), '_');
+                for _ in seq_vec.iter() {
+                    sum += score(seq.chars().nth(i).unwrap(), '_');
                 }
                 sum += matrix[i][j - 1].val;
                 sum
@@ -146,7 +144,7 @@ fn align_seqs(mut seq_vec1: Vec<String>, mut seq_vec2: Vec<String>) {
 
             uppr_val = {
                 let mut sum = 0;
-                for sec in seq_vec1.iter() {
+                for sec in seq_vec.iter() {
                     sum += score('_', sec.chars().nth(j).unwrap());
                 }
                 sum += matrix[i - 1][j].val;
@@ -166,12 +164,12 @@ fn align_seqs(mut seq_vec1: Vec<String>, mut seq_vec2: Vec<String>) {
         }
     }
 
-    print_matrix(&seq_vec1, &seq_vec2, &matrix);
+    print_matrix(&seq_vec, &seq, &matrix);
 
     /********* Restore sequences **********/
-    seq_vec2.remove(0);
+    seq.remove(0);
 
-    for sec in seq_vec1.iter_mut() {
+    for sec in seq_vec.iter_mut() {
         sec.remove(0);
     }
     /**************************************/
@@ -188,12 +186,12 @@ fn align_seqs(mut seq_vec1: Vec<String>, mut seq_vec2: Vec<String>) {
                 j -= 1;
             }
             Left => {
-                let term = seq_vec2.split_off(j);
-                seq_vec2 = format!("{}_{}", seq_vec2, term);
+                let term = seq.split_off(j);
+                seq = format!("{}_{}", seq, term);
                 j -= 1;
             }
             Up => {
-                for mut sec in seq_vec1.iter_mut() {
+                for mut sec in seq_vec.iter_mut() {
                     let term = sec.split_off(i);
                     *sec = format!("{}_{}", sec, term);
                 }
@@ -205,8 +203,160 @@ fn align_seqs(mut seq_vec1: Vec<String>, mut seq_vec2: Vec<String>) {
         }
     }
 
-    println!("\n> {}", seq_vec2);
+    println!("\n> {}", seq);
+    for sec in seq_vec.iter() {
+        println!("> {}", sec)
+    }
+}
+
+fn align_alignments(mut seq_vec1: Vec<String>, mut seq_vec2: Vec<String>) {
+    /********* Initialize sequences **********/
+    for sec in seq_vec1.iter_mut() {
+        *sec = String::from("_") + sec.as_ref();
+    }
+    for sec in seq_vec2.iter_mut() {
+        *sec = String::from("_") + sec.as_ref();
+    }
+    /********* score function **********/
+    fn score(a: char, b: char) -> f32 {
+        if a == '_' || b == '_' {
+            0.0
+        } else if a == b {
+            2.0
+        } else {
+            1.0
+        }
+    }
+    /********* Initialize matrix **********/
+    let len_1 = seq_vec1[0].len();
+    let len_2 = seq_vec2[0].len();
+    let n_seqs = (seq_vec1.len() + seq_vec2.len()) as f32;
+
+    let mut matrix: Vec<Vec<Score>> = vec![vec![Score::new(); len_1]; len_2];
+
+    for j in 1..len_1 {
+        matrix[0][j].dir = Left;
+    }
+    for i in 1..len_2 {
+        matrix[i][0].dir = Up;
+    }
+    /**************************************/
+    let mut diag_val;
+    let mut left_val;
+    let mut uppr_val;
+
+    for i in 1..len_2 {
+        for j in 1..len_1 {
+            // println!("({}, {})", j, i);
+            diag_val = {
+                let mut sum = 0.0;
+                for sec1 in seq_vec1.iter() {
+                    for sec2 in seq_vec2.iter() {
+                        sum += score(sec1.chars().nth(j).unwrap(), sec2.chars().nth(i).unwrap());
+                        // println!("{}:{}", sec1.chars().nth(j).unwrap(), sec2.chars().nth(i).unwrap());
+                    }
+                }
+                sum /= n_seqs;
+                sum += matrix[i - 1][j - 1].val;
+                // println!("D: {}\n", sum);
+                sum
+            };
+
+            left_val = {
+                let mut sum = 0.0;
+                for sec1 in seq_vec1.iter() {
+                    for sec2 in seq_vec2.iter() {
+                        sum += score(sec1.chars().nth(j-1).unwrap(), sec2.chars().nth(i).unwrap());
+                        // println!("{}:{}", sec1.chars().nth(j-1).unwrap(), sec2.chars().nth(i).unwrap());
+                    }
+                }
+                sum /= n_seqs;
+                // sum += matrix[i][j - 1].val;
+                // println!("L: {}\n", sum);
+                sum
+            };
+
+            uppr_val = {
+                let mut sum = 0.0;
+                for sec1 in seq_vec1.iter() {
+                    for sec2 in seq_vec2.iter() {
+                        sum += score(sec1.chars().nth(j).unwrap(), sec2.chars().nth(i-1).unwrap());
+                        // println!("{}:{}", sec1.chars().nth(j).unwrap(), sec2.chars().nth(i-1).unwrap());
+                    }
+                }
+                sum /= n_seqs;
+                // sum += matrix[i - 1][j].val;
+                // println!("U: {}\n", sum);
+                sum
+            };
+
+            if diag_val >= left_val {
+                if diag_val >= uppr_val {
+                    matrix[i][j].val = diag_val;
+                    matrix[i][j].dir = Diagonal;
+                } else {
+                    matrix[i][j].val = uppr_val;
+                    matrix[i][j].dir = Up;
+                }
+            } else {
+                if left_val >= uppr_val {
+                    matrix[i][j].val = left_val;
+                    matrix[i][j].dir = Left;
+                } else {
+                    matrix[i][j].val = uppr_val;
+                    matrix[i][j].dir = Up;
+                }
+            }
+        }
+    }
+
+    print_matrix(&seq_vec1, &seq_vec2, &matrix);
+
+    /********* Restore sequences **********/
+    for sec in seq_vec1.iter_mut() {
+        sec.remove(0);
+    }
+    for sec in seq_vec2.iter_mut() {
+        sec.remove(0);
+    }
+    /**************************************/
+
+    let mut i = len_2 - 1;
+    let mut j = len_1 - 1;
+    let mut dir: Direction;
+    while i != 0 || j != 0 {
+        dir = matrix[i][j].dir.clone();
+        println!("{:?}", dir);
+
+        match dir {
+            Diagonal => {
+                i -= 1;
+                j -= 1;
+            }
+            Left => {
+                for mut sec in seq_vec2.iter_mut() {
+                    let term = sec.split_off(i);
+                    *sec = format!("{}_{}", sec, term);
+                }
+                j -= 1;
+            }
+            Up => {
+                for mut sec in seq_vec1.iter_mut() {
+                    let term = sec.split_off(j);
+                    *sec = format!("{}_{}", sec, term);
+                }
+                i -= 1;
+            }
+            Undef => {
+                break;
+            }
+        }
+    }
+
     for sec in seq_vec1.iter() {
+        println!("> {}", sec)
+    }
+    for sec in seq_vec2.iter() {
         println!("> {}", sec)
     }
 }
@@ -243,7 +393,16 @@ pub fn tps_alignment(seqs: Vec<&str>) {
     }
 
     my_vec.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap());
-    println!("{:?}", my_vec);
+    // println!("{:?}", my_vec);
+
+    align_alignments(vec![String::from("AAAC"), String::from("_GAC")],
+                     vec![String::from("AGC"), String::from("ACC")]);
+
+    align_seqs_seq(vec![String::from("ACTCAT"), String::from("AGTCAT")],
+                   String::from("ACGTCCT"));
+
+    // align_alignments(vec![String::from("TGTAAC")],
+    //                  vec![String::from("TGTAC")]);
 
     // let tree = TpsTree::new();
     // let joint1 = TpsJoint::from_strings(seqs[my_vec[0].1], seqs[my_vec[0].2]);
