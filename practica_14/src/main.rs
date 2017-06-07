f434371992d40134d962d2019e8ab6ced48f470a
Elías Puma <elpumitaelias@gmail.com>
2017-06-07 04:57:52 -0500

    Added input for solver

--- practica_14/src/main.rs
+++ practica_14/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    println!("run:");
+
+    let pob_size = 4;
+    let crom_size = 5;
+    let iterations = 30;
+    let crossover_prob = 0.9;
+    let crossover_point = 3;
+    let mutation_prob = 0.1;
+
+    println!("Tamaño de la Población: {}", pob_size);
+    println!("Tamaño de los Cromosomas: {}", crom_size);
+    println!("Cantidad de Iteraciones: {}", iterations);
+    println!("Probabilidad de Cruzamiento: {}", crossover_prob);
+    println!("Cruzamiento de un Punto - Punto {}", crossover_point);
+    println!("Probabilidad de Mutación: {}", mutation_prob);
+    println!("Mutación Simple");
 }
