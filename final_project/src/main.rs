c7fb5a2f42f18f4b43bcf9b6796a737c60e187cc
Elías Puma <elpumitaelias@gmail.com>
2017-07-14 08:57:10 -0500

    Added files for final project

---
+++ final_project/src/main.rs
@@ -0,0 +1,12 @@
+fn get_sequences(path: &str) -> Vec<String> {
+    Vec::new()
+}
+
+fn mult_seq_alignment(input: &[String]) {
+    println!("Hello, world!");
+}
+
+fn main() {
+    let input = get_sequences("input/MSA_16507.txt");
+    mult_seq_alignment(&input);
+}
